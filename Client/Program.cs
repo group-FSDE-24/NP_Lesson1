a5ed8a7e2997b6685a12e5e4d685643599ea106f
karimzade_k <karimzade_k@STHQ0118-01>
2025-09-30 21:28:07 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+        var ip = IPAddress.Parse("127.0.0.1");
+
+        var port = 8005;
+
+        // EndPoint -> ip + port -> 127.0.0.1:8005
+
+        var serverEndPoint = new IPEndPoint(ip, port);
+
+        try
+        {
+            client.Connect(serverEndPoint);
+
+            if (client.Connected)
+            {
+                Console.WriteLine("Connected to Server...");
+
+                Task.Run(() =>
+                {
+                    var buffer = new byte[1024];
+                    var len = 0;
+                    var msj = string.Empty;
+
+                    while (true)
+                    {
+                        len = client.Receive(buffer);
+                        msj = Encoding.Default.GetString(buffer, 0, len);
+
+                        Console.WriteLine(msj);
+                    }
+
+                });
+
+                while (true)
+                {
+                    var msj = Console.ReadLine();
+                    var buffer = Encoding.Default.GetBytes(msj);
+
+                    client.Send(buffer);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Server can not be connected....");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Server can not be connected....");
+            Console.WriteLine($"Ex: {ex.Message}");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server;
+
+/*
+
+Server Qurmaq ucun mene neler lazimdir:
+
+1. OS           -> operation system
+2. Web Service  -> Apache, IIS ( Windows ), Nginx
+3. Traslator    -> C#, Php, Java
+ 
+*/
+
+
+class Program
+{
+    static void Main()
+    {
+        var listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+        var ip = IPAddress.Parse("127.0.0.1");
+
+        var port = 8005;
+
+        // EndPoint -> ip + port -> 127.0.0.1:8005
+
+        var ep = new IPEndPoint(ip, port);
+
+        listener.Bind(ep);
+
+        // |
+        var backlog = 1;
+
+        listener.Listen(backlog);
+
+        Console.WriteLine($"Server listen on {listener.LocalEndPoint}");
+
+        while (true)
+        {
+            listener.Accept();
+
+            Console.WriteLine($"{listener.RemoteEndPoint} connected...");
+        }
+
+
+
+
+
+
+    }
+}
